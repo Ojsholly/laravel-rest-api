f04af1afb6bd5c6216590003990ab90ab26bd1ff
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2020-05-26 05:42:41 +0100

    Store and Update Records
    Created endpoints for storing and updating records.

--- app/Person.php
+++ app/Person.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     //
+
+    protected $fillable = [
+
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'city',
+
+    ];
 }
