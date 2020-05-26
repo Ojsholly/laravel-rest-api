c51233bee07f7be4f467d36eea26579fcae0ff27
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2020-05-26 21:43:51 +0100

    Api Versioning

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('/person', 'PersonController')->only(['show', 'destroy', 'update', 'store']);
+
+    Route::apiResource('/people', 'Api\v1\PersonController')->only('index');
 });
 
-Route::apiResource('/person', 'PersonController');
+Route::prefix('v2')->group(function() {
+
+    Route::apiResource('/person','Api\v2\PersonController')->only('show');
+
+});
